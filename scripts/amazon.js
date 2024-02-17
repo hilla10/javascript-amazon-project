b9d2b433bf7f843e409d581813e6b54df0c530e6
hilla10 <hillaman592@gmail.com>
2024-02-16 17:53:29 -1100

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -72,6 +72,15 @@ addButton.forEach((button) => {
             });
         }
 
-        console.log(cart);
+        let cartQuantity = 0;
+
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+
+
+        document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
     });
 });
+
